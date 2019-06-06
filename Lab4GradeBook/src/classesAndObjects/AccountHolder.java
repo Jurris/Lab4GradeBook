279107e57081fac579ba3ee83e84d05dad41c01b
RicardoM <jurris.dev@gmail.com>
2019-06-06 02:50:52 -0700

    Created an AccountHolder Class and an AccountHolderObjects Class

---
+++ Lab4GradeBook/src/classesAndObjects/AccountHolder.java
@@ -0,0 +1,78 @@
+package classesAndObjects;
+
+public class AccountHolder {
+
+	String fname;
+	int gradepoints;
+	String letterGrade;
+	boolean letterA;
+	boolean letterAPlus;
+	boolean letterAMinus;
+	boolean letterB;
+	boolean letterBPlus;
+	boolean letterBMinus;
+	boolean letterC;
+	boolean letterCPlus;
+	boolean letterCminus;
+	boolean letterD;
+	boolean letterDPlus;
+	boolean letterDMinus;
+	boolean letterF;
+
+	public void testForLetterGradeEarned() {
+
+		if (gradepoints >= 388) {
+			letterAPlus = true;
+			letterGrade = "A+";
+			//System.out.println("A+");
+		} else if (gradepoints >= 372) {
+			letterA = true;
+			letterGrade = "A";
+			//System.out.println("A");
+		} else if (gradepoints >= 360) {
+			letterAMinus = true;
+			letterGrade = "A-";
+			//System.out.println("A-");
+		} else if (gradepoints >= 340) {
+			letterBPlus = true;
+			letterGrade = "B+";
+			//System.out.println("B+");
+		} else if (gradepoints >= 336) {
+			letterB = true;
+			letterGrade = "B";
+			//System.out.println("B");
+		} else if (gradepoints >= 320) {
+			letterBMinus = true;
+			letterGrade = "B-";
+			//System.out.println("B-");
+		} else if (gradepoints >= 308) {
+			letterCPlus = true;
+			letterGrade = "C+";
+			//System.out.println("C+");
+		} else if (gradepoints >= 280) {
+			letterC = true;
+			letterGrade = "C";
+			//System.out.println("C");
+		} else if (gradepoints >= 268) {
+			letterDPlus = true;
+			letterGrade = "C-";
+			//System.out.println("C-");
+		} else if (gradepoints >= 252) {
+			letterD = true;
+			letterGrade = "D+";
+			//System.out.println("D+");
+		} else if (gradepoints >= 240) {
+			letterDMinus = true;
+			letterGrade = "D";
+			//System.out.println("D");
+		} else if (gradepoints >= 0) {
+			letterF = true;
+			letterGrade = "F";
+			//System.out.println("F");
+
+		}
+		
+	
+
+	}
+}
---
+++ Lab4GradeBook/src/classesAndObjects/AccountHolderOjects.java
@@ -0,0 +1,43 @@
+package classesAndObjects;
+
+
+public class AccountHolderOjects {
+
+	public static void main(String[] args) {
+		AccountHolder john = new AccountHolder();
+		AccountHolder hamid = new AccountHolder();
+		AccountHolder tracy = new AccountHolder();
+		AccountHolder albert = new AccountHolder();
+		AccountHolder ana = new AccountHolder();
+		
+		john.fname="John";
+		john.gradepoints=308;
+		john.testForLetterGradeEarned();
+		System.out.println(john.fname + " earned a " + john.letterGrade+".");
+		
+		hamid.fname="Hamid";
+		hamid.gradepoints=370;
+		hamid.testForLetterGradeEarned();
+		System.out.println(hamid.fname + " earned a " + hamid.letterGrade+".");
+		
+		tracy.fname="Tracy";
+		tracy.gradepoints=331;
+		tracy.testForLetterGradeEarned();
+		System.out.println(tracy.fname + " earned a " + tracy.letterGrade+".");
+		
+		albert.fname="Albert";
+		albert.gradepoints=336;
+		albert.testForLetterGradeEarned();
+		System.out.println(albert.fname + " earned a " + albert.letterGrade+".");
+		
+		ana.fname="Ana";
+		ana.gradepoints=321;
+		ana.testForLetterGradeEarned();
+		System.out.println(ana.fname + " earned a " + ana.letterGrade+".");
+		
+		System.out.println("The average grade in the class is "+(john.gradepoints+hamid.gradepoints+tracy.gradepoints+albert.gradepoints+ana.gradepoints)/5+" and the average grade is B-.");
+		
+
+	}
+
+}
